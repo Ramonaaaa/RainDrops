057d6a0841748b5976a527d8741c3b7d8f4efb7d
Ramona Mihai <ramona.mihai87@yahoo.com>
2018-12-16 13:38:16 +0200

    Rain drops

---
+++ RainDrops/RainDrops/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace RainDrops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"12 => {RainDrops(12)}");
+            Console.WriteLine($"30 => {RainDrops(30)}");
+            Console.WriteLine($"34 => {RainDrops(34)}");
+            Console.WriteLine($"105 => {RainDrops(105)}");
+        }
+
+        public static string RainDrops(int number)
+        {
+            string output = "";
+
+            if (number % 3 == 0)
+            {
+                output += "Pling";
+            }
+            if (number % 5 == 0)
+            {
+                output += "Plang";
+            }
+            if (number % 7 == 0)
+            {
+                output += "Plong";
+            }
+            if (output == "")
+            {
+                output = number.ToString();
+            }
+
+            return output;
+        }
+    }
+}
